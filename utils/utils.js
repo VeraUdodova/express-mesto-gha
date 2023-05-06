5355f2bef95f71adb71d4fc6f0926d367d58b8f7
Vera Udodova <udodowa.vera@yandex.ru>
2023-05-06 11:17:09 +0300

    fix tests

--- utils/utils.js
+++ utils/utils.js
@@ -5,6 +5,7 @@ const HTTP_201 = 201;
 const HTTP_400 = 400;
 const HTTP_409 = 409;
 const HTTP_401 = 401;
+const HTTP_403 = 403;
 const HTTP_404 = 404;
 const HTTP_500 = 500;
 
@@ -65,6 +66,7 @@ module.exports = {
   HTTP_201,
   HTTP_400,
   HTTP_401,
+  HTTP_403,
   HTTP_404,
   HTTP_409,
   HTTP_500,
